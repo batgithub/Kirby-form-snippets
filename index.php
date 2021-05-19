7a95dab342b28433eb24a69188c71644f380b1b9
baptiste lhopitault <lhopitault.baptiste@gmail.com>
2021-05-19 16:48:27 +0200

    fix snippet declaration

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 Kirby::plugin('baptiste/kirby-form-snippets', [
     'snippets' => [
-        'form/text' => __DIR__ . '/snippets/fields/text.php',
+        'form/input' => __DIR__ . '/snippets/fields/input.php',
         'form/textarea' => __DIR__ . '/snippets/fields/text.php',
         'form/honeypot' => __DIR__ . '/snippets/fields/honeypot.php',
         'form/notif' => __DIR__ . '/snippets/fields/notif.php',

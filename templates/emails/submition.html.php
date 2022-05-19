5cd0e346317ef89381747cf91888e6f644a40f92
baptiste lhopitault <lhopitault.baptiste@gmail.com>
2022-05-19 12:51:41 +0200

    improve email template

--- templates/emails/submition.html.php
+++ templates/emails/submition.html.php
@@ -1,38 +1,239 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1>Nouvelle soumission au formulaire <?= $formName ?></h1>
-        <p><?= $date ?></p>
-        <table>
-            <thead>
-                <tr>
-                    <th>Question</th>
-                    <th>Réponse</th>
-                </tr>
-            </thead>
-                <?php foreach($datas as $field => $data): ?>
-                        <tr>
-                            <td><?= $data['label'] ?></td>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office">
 
-                            <?php if($data['value'] !== ""): ?>
-                                <td>
-                                    <?php if($data['input'] == 'checkbox-group'): ?>
-                                        <ul>
-                                            <?php foreach( $data['value'] as $item): ?>
-                                                <li><?= Str::replace($item, '-', ' ') ?></li>
-                                            <?php endforeach ?>
-                                        </ul>
-        
-                                    <?php else : ?>
-                                        <?= $data['value'] ?>
-                                    <?php endif; ?>
-                                </td>
-                            <?php else: ?>
-                                <td>----</td>
-                            <?php endif; ?>
-                        </tr>            
-                <?php endforeach ?>
-        </table>
-    </body>
+<head>
+    <title>Nouvelle soumission</title>
+    <!--[if !mso]><!-- -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <style type="text/css">
+        #outlook a {
+            padding: 0;
+        }
+
+        .ReadMsgBody {
+            width: 100%;
+        }
+
+        .ExternalClass {
+            width: 100%;
+        }
+
+        .ExternalClass * {
+            line-height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
+
+        table,
+        td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
+
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
+
+        p {
+            display: block;
+            margin: 13px 0;
+        }
+    </style>
+    <!--[if !mso]><!-->
+    <style type="text/css">
+        @media only screen and (max-width:480px) {
+            @-ms-viewport {
+                width: 320px;
+            }
+
+            @viewport {
+                width: 320px;
+            }
+        }
+    </style>
+    <!--<![endif]-->
+    <!--[if mso]>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+    <!--[if lte mso 11]>
+        <style type="text/css">
+          .outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+    <style type="text/css">
+        @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+                width: 100% !important;
+                max-width: 100%;
+            }
+        }
+    </style>
+    <style type="text/css"></style>
+    <style type="text/css">
+        ul {
+            margin-top: 0px;
+            padding-left: 24px;
+        }
+    </style>
+</head>
+
+<body style="background-color:#FBFBFB;">
+    <div
+        style="display:none;font-size:1px;color:#ffffff;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;">
+        Nouvelle soumisssion sur</div>
+    <div style="background-color:#FBFBFB;">
+        <!-- Image Header -->
+        <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+                <tbody>
+                    <tr>
+                        <td
+                            style="direction:ltr;font-size:0px;padding:28px 0px 0px;text-align:center;vertical-align:top;">
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                            <div class="mj-column-per-100 outlook-group-fix"
+                                style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                    style="vertical-align:top;" width="100%"></table>
+                            </div>
+                            <!--[if mso | IE]></td></tr></table><![endif]-->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+                <tbody>
+                    <tr>
+                        <td
+                            style="direction:ltr;font-size:0px;padding:28px 8px 32px;text-align:center;vertical-align:top;">
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:584px;" ><![endif]-->
+                            <div class="mj-column-per-100 outlook-group-fix"
+                                style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                    style="vertical-align:top;" width="100%">
+                                    <tr>
+                                        <td align="left" style="font-size:0px;padding:0px;word-break:break-word;">
+                                            <div
+                                                style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:24px;font-weight:500;line-height:32px;text-align:left;color:#2B2B2B;">
+                                                Formulaire <?= $formName ?></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left"
+                                            style="font-size:0px;padding:8px 0px 0px;word-break:break-word;">
+                                            <div
+                                                style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:16px;font-weight:400;line-height:24px;text-align:left;color:#2B2B2B;">
+                                                <?= $date ?></div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <!--[if mso | IE]></td></tr></table><![endif]-->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><![endif]-->
+        <!-- Block step -->
+        <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:white;background-color:white;Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                style="background:white;background-color:white;width:100%;">
+                <tbody>
+                    <tr>
+                        <td
+                            style="border:2px solid #CCC;direction:ltr;font-size:0px;padding:32px 32px;text-align:center;vertical-align:top;">
+
+                            <?php foreach($datas as $field => $data): ?>
+
+
+
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" width="600px" ><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:532px;" width="532" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+                            <div style="Margin:0px auto;max-width:532px;">
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                    style="width:100%;">
+                                    <tbody>
+                                        <tr>
+                                            <td
+                                                style="direction:ltr;font-size:0px;padding:24px 0px 10px;text-align:center;vertical-align:top;">
+                                                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:532px;" ><![endif]-->
+                                                <div class="mj-column-per-100 outlook-group-fix"
+                                                    style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        role="presentation" style="vertical-align:top;" width="100%">
+                                                        <tr>
+                                                            <td align="left"
+                                                                style="font-size:0px;padding:0px;word-break:break-word;">
+                                                                <div
+                                                                    style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:14px;font-weight:bold;line-height:24px;text-align:left;color:#2B2B2B;">
+                                                                    <?= $data['label'] ?></div>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="left"
+                                                                style="font-size:0px;padding:0px;word-break:break-word;">
+                                                                <div
+                                                                    style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;font-size:18px;font-weight:400;line-height:24px;text-align:left;color:#2B2B2B;">
+                                                                    <?php if($data['value'] !== ""): ?>
+
+                                                                        <?php if($data['input'] == 'checkbox-group'): ?>
+                                                                            <ul>
+                                                                                <?php foreach( $data['value'] as $item): ?>
+                                                                                <li><?= Str::replace($item, '-', ' ') ?></li>
+                                                                                <?php endforeach ?>
+                                                                            </ul>
+
+                                                                        <?php else : ?>
+                                                                            <?= $data['value'] ?>
+                                                                        <?php endif; ?>
+                                                                    <?php else: ?>
+                                                                    ----
+                                                                    <?php endif; ?>
+                                                                
+                                                                
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </div>
+                                                <!--[if mso | IE]></td></tr></table><![endif]-->
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <?php endforeach ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><![endif]-->
+    </div>
+</body>
+
 </html>
-
